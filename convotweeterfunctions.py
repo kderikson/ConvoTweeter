e90908caa73b47214cb24add9983650b8506ffa0
Kyle Erikson <kderikson@gmail.com>
2015-01-12 20:43:18 -0500

    Moving functions to separate file

--- convotweeterfunctions.py
+++ convotweeterfunctions.py
@@ -1,37 +1,16 @@
-from TwitterAPI import TwitterAPI
-consumer_key = "xEcJxElLrGa2dmeJqQhW9hL7K"
-consumer_sec = "QOl78B7RhHoY4Js34zyHBihBqKl52bMyQSw46wbO3eb3jjBJF9"
-access_key = "2950254105-FsJMleHL8q8aSE3wJj0I0uD8oeSg8M1IO9pJqv8"
-access_sec = "KDmSQM6bgn0T1dJUQjU5XKu1uZ7PMljQVAJZDND51PDjq"
-
-api = TwitterAPI(consumer_key, consumer_sec, access_key, access_sec)
-
-count = 0
-
 import requests
 import re
-from bs4 import BeautifulSoup
 import time
 from datetime import date
 from time import strftime
-data = requests.get("https://www.liberty.edu/index.cfm?PID=2586")
-today = date.today()
-today = today.strftime("%m/%d")
-print today
 
-# Todays Date
-todays_date = str(today)
-todays_date = "9/17"
+def RetrieveTodaysDate():
+    today = date.today()
+    today = today.strftime("%m/%d")
+    today = "1/23"
+    return today
 
-# Grab the HTML content
-html = data.text
-soup = BeautifulSoup(html)
-
-# Get the first speaker
-curr_speaker = soup.h1.next_sibling.next_sibling
-print curr_speaker
-
-def NotFound():
+def NotFound(curr_speaker, todays_date):
     speaker_date = re.search('[0-9]+/[0-9]+', str(curr_speaker)).group(0)
     print "Speaker date " + speaker_date
     print "Today's date " + todays_date
@@ -42,12 +21,7 @@ def NotFound():
         return True
 
 
-while NotFound():
-    # Go to the next speaker in the list
-    temp = curr_speaker.next_sibling
-    curr_speaker = temp.next_sibling
-
-def ComposeTweet():
+def ComposeTweet(curr_speaker, todays_date):
     # Speakers Name
     speaker_name = curr_speaker.a.string
     print speaker_name
@@ -79,10 +53,8 @@ def ComposeTweet():
 
     return composed_tweet
 
-def TweetTweet():
-    tweet = api.request('statuses/update', {'status': ComposeTweet()})
 
-    print('SUCCESS' if tweet.status_code == 200 else 'FAILURE')
+def TweetTweet(tweet):
+    post = api.request('statuses/update', {'status': tweet})
 
-
-TweetTweet()
+    print('SUCCESS' if post.status_code == 200 else 'FAILURE')
